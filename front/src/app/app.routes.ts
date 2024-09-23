dd63b566b88176d8c7aca88d8113b2849bb5be7b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-09-23 12:12:25 +0200

    dev: added home and legal

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './routes/home/home.component';
+import { LegalComponent } from './routes/legal/legal.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'legal', component: LegalComponent },
+  //   { path: 'stock', loadChildren: () => StockModule },
+];
---
+++ front/src/app/routes/legal/legal.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-legal',
+  templateUrl: './legal.component.html',
+  styleUrls: ['./legal.component.scss'],
+  standalone: true,
+})
+export class LegalComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
