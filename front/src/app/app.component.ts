c6ce270bd799d0164898c9d27a09978ac55303d8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-09-23 11:54:33 +0200

    dev: added layout

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { LayoutModule } from './layout/layout.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [LayoutModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'front';
