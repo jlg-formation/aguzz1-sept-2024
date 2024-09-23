5eb3490ac900a8d94ef500f24b3a6753b9d079a8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-09-23 14:22:46 +0200

    dev: completely migrated to 18

--- front/src/app/routes/home/home.component.ts
+++ front/src/app/routes/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
+  imports: [RouterLink],
 })
 export class HomeComponent implements OnInit {
   constructor() {}
