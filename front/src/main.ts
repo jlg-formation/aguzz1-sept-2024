7b9df0d72e6b92ddaae87e14a8aca9f4118814b6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-09-24 12:36:08 +0200

    dev: no console log

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { isDevMode } from '@angular/core';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+if (!isDevMode()) {
+  console.log = () => {};
+  console.error = () => {};
+  console.warn = () => {};
+}
+
+bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
