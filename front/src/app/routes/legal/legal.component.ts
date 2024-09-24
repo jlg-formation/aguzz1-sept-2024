63cfb0f81ea8c0e38b5b2dc0b6783d074cc05d3b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-09-24 11:50:54 +0200

    dev: added takeuntildestroy

--- front/src/app/routes/legal/legal.component.ts
+++ front/src/app/routes/legal/legal.component.ts
@@ -1,13 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription, tap } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-legal',
   templateUrl: './legal.component.html',
   styleUrls: ['./legal.component.scss'],
   standalone: true,
+  imports: [AsyncPipe],
 })
 export class LegalComponent implements OnInit {
-  constructor() {}
+  chrono = 0;
+
+  constructor() {
+    interval(1000)
+      .pipe(
+        tap(x => {
+          console.log('x: ', x);
+          this.chrono = x;
+        }),
+        takeUntilDestroyed()
+      )
+      .subscribe();
+  }
 
   ngOnInit(): void {}
 }
